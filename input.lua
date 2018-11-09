6390b52aafdd30b710746a6c5128c62ae6bffc82
lbrentcarpenter <garenarterius1@gmail.com>
2018-11-09 11:49:49 +0000

    start input file

---
+++ input.lua
@@ -0,0 +1,14 @@
+-- input interface
+-- TODO:
+--  * joystick
+--  * keyboard
+--  * custom bindings
+
+-- private keybinds map
+local map = {}
+
+local input = {
+	bind = function(key, effect) end
+}
+return input
+
